247559881b1f36c301109ad162cd7d51e3dcd562
Al Gabriel <jaworski.orig@gmail.com>
2022-12-29 22:55:58 +0800

    add: flutter and android studio conditions

--- macos.sh
+++ macos.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+clear 
 
 echo "=============================================================================="
 echo "||  IIIIIIII I          I    I  IIIIIIIIII IIIIIIIIII IIIIIIII IIIIIIIIII   ||"
@@ -10,8 +11,21 @@ echo "||  I        I          I    I      I          I      I        I          
 echo "||  I        IIIIIIIII  IIIIII      I          I      IIIIIIII I          I ||"
 echo "=============================================================================="
 
+
+# Statement prompt
+echo "Do you agree to install these dependencies on your system? \n \n Dependencies: \n - Xcode CLI \n - Homebrew \n - Flutter \n - Android Studio" ; sleep 4
+
+# Prompt the user to press the "return" key to agree
+echo ""
+read -p "Press return / enter to agree: "
+
+# Print a message indicating that the user agreed
+clear
+
+
 # Check if Dependencies are installed
-echo "Checking Dependencies" ; sleep 6
+echo "Installing Dependencies:" 
+echo "----------------------- \n" ; sleep 6
 
 # Xcode CLI Tools 
 if xcode-select --print-path > /dev/null; then
@@ -21,8 +35,11 @@ else
   # Xcode Command Line Tools are not installed
   echo "Xcode Command Line Tools are not installed" ; sleep 3
   clear
+echo "Installing Dependencies:" 
+echo "----------------------- \n" ;
   echo "Installing Xcode Command Line Tools" ; sleep 3
-  code-select --install
+  xcode-select --install
+  clear
 fi
 
 #Homebrew
@@ -33,22 +50,40 @@ else
   # Homebrew is not installed
   echo "Homebrew is not installed" ; sleep 3
   clear
+  echo "Installing Dependencies:" 
+  echo "----------------------- \n" ; 
   echo "Installing Homebrew..."
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
-
+  clear
 fi
 
+#Flutter
+if command -v flutter >/dev/null 2>&1; then
+  # Flutter is installed
+  echo "Flutter is installed" ; sleep 3
+else
+  # Flutter is not installed
+  echo "Flutter is not installed" ; sleep 3
+  clear
+    echo "Installing Dependencies:" 
+    echo "----------------------- \n" ;
+  echo "Installing Flutter..."
+  brew install --cask flutter
+  clear
+fi
 
+# Android Studio
+if [ -d "/Applications/Android Studio.app" ]; then
+  echo "Android Studio is installed"
+else
+  echo "Android Studio is not installed" ;sleep 3
+  clear
+echo "Installing Dependencies:" 
+echo "----------------------- \n" ;
+  echo "Installing Android Studio..."
+  brew install --cask android-studio
+  clear
+fi
 
-zenity --info --title="Message" --icon-name="dialog-information" --text="This is a message with an OK button"
-
-
-zenity --progress --title="Installing Flutter" --text="Installing Flutter..." --pulsate &
-
-pid=$!
-
-brew install flutter ; sleep 1 
-
-kill $pid ; sleep 1
-
-zenity --info --title="Formula installed" --text="The formula was successfully installed"
+clear
+echo "Installation Complete... \n Open Android Studio to install necessary SDKs \n and run flutter doctor afterwards in your terminal"
