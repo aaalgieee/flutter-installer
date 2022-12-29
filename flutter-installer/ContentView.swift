9b23a3bbe7af400596a8a72a8ff04d1e32dab5d4
Al Gabriel <jaworski.orig@gmail.com>
2022-12-29 20:10:29 +0800

    Initial Commit

---
+++ flutter-installer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  flutter-installer
+//
+//  Created by Al Gabriel on 12/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ flutter-installer/flutter_installerApp.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_installerApp.swift
+//  flutter-installer
+//
+//  Created by Al Gabriel on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct flutter_installerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
