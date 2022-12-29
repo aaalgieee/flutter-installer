dc65079885bf2c6640729e62f93fc191f7129b90
Al Gabriel <jaworski.orig@gmail.com>
2022-12-29 21:41:49 +0800

    add: homebrew conditions

--- macos.sh
+++ macos.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+echo "=============================================================================="
+echo "||  IIIIIIII I          I    I  IIIIIIIIII IIIIIIIIII IIIIIIII IIIIIIIIII   ||"
+echo "||  I        I          I    I      I          I      I        I         I  ||"
+echo "||  I        I          I    I      I          I      I        I          I ||"
+echo "||  IIIII    I          I    I      I          I      IIIII    I         I  ||"
+echo "||  I        I          I    I      I          I      I        IIIIIIIIIII  ||"
+echo "||  I        I          I    I      I          I      I        I          I ||"
+echo "||  I        IIIIIIIII  IIIIII      I          I      IIIIIIII I          I ||"
+echo "=============================================================================="
 
 # Check if Dependencies are installed
 echo "Checking Dependencies" ; sleep 6
@@ -7,25 +16,27 @@ echo "Checking Dependencies" ; sleep 6
 # Xcode CLI Tools 
 if xcode-select --print-path > /dev/null; then
   # Xcode Command Line Tools are installed
-  echo "Xcode Command Line Tools are installed"
+  echo "Xcode Command Line Tools are installed" ; sleep 3
 else
   # Xcode Command Line Tools are not installed
   echo "Xcode Command Line Tools are not installed" ; sleep 3
+  clear
+  echo "Installing Xcode Command Line Tools" ; sleep 3
+  code-select --install
 fi
 
-#Hombrew
+#Homebrew
 if which brew > /dev/null; then
   # Homebrew is installed
-  echo "Homebrew is installed"
+  echo "Homebrew is installed" ; sleep 3
 else
   # Homebrew is not installed
   echo "Homebrew is not installed" ; sleep 3
   clear
   echo "Installing Homebrew..."
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
 
 fi
-
-# Install Home
 
 
 
